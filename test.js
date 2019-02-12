a087a6b249fb4a45564c466dbb22e7a898ca33c9
Daniel Wang <danielwpz@gmail.com>
2019-02-12 11:21:19 -0800

    add http fail tests

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ const okChecker = new Checker('ok', () => Promise.resolve(true), 100);
 const throwChecker = new Checker('throw', () => Promise.reject('Bad happened'), 100);
 const timeoutChecker = new Checker('timeout', () => new Promise((res, rej) => {}), 1000);
 const httpChecker = new HTTPChecker('google', 'https://www.google.com/');
-const httpFa
+const httpFailChecker = new HTTPChecker('404', 'https://www.github.com/foo/bar');
 
-const healthcheck = new Healthcheck([okChecker, throwChecker, timeoutChecker, httpChecker]);
+const healthcheck = new Healthcheck([okChecker, throwChecker, timeoutChecker, httpChecker, httpFailChecker]);
 healthcheck.run().then(console.log);
